2b95ac8dbbba0d39132293dfac1b812dc2d26d19
Darren Meehan <hi@drn.ie>
2024-06-03 21:13:16 +0100

    Add structured logging

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
 use axum::Router;
 use std::net::SocketAddr;
+use tower_http::trace;
 use tower_http::{services::ServeDir, trace::TraceLayer};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::{event, Level};
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "drn_ie_server=debug,tower_http=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
+    let file_appender = RollingFileAppender::builder()
+        .rotation(Rotation::DAILY)
+        .filename_prefix("drn-ie.log")
+        .build("log") // write log files to the 'log' directory
+        .expect("failed to initialize rolling file appender");
+
+    let (file_non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    let (stdout_non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
+    tracing_subscriber::fmt()
+        .with_writer(file_non_blocking)
+        .with_writer(stdout_non_blocking)
         .init();
 
-    let app = Router::new().nest_service("/", ServeDir::new("public"));
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("public"))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
+        );
     let port = 3001;
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    event!(
+        Level::INFO,
+        "Application will listen at {}",
+        listener.local_addr().unwrap()
+    );
     axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
         .unwrap();

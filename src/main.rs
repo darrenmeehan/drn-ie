fd77a790e0acb981b7b25976c3c75c0c9ce6ecb1
Darren Meehan <hi@drn.ie>
2024-06-03 20:56:01 +0100

    Change to basic HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,24 @@
-use axum::{routing::{get, post}, Router};
-use tower_http::trace::{self, TraceLayer};
-use tracing::{event, span, Level};
-use tracing_appender::rolling::{RollingFileAppender, Rotation};
+use axum::Router;
+use std::net::SocketAddr;
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let file_appender = RollingFileAppender::builder()
-        .rotation(Rotation::DAILY)
-        .filename_prefix("drn-ie.logging")
-        .build("log") // write log files to the 'log' directory
-        .expect("failed to initialize rolling file appender");
-
-    let (file_non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    let (stdout_non_blocking, _guard) = tracing_appender::non_blocking(std::io::stdout());
-    tracing_subscriber::fmt()
-        .with_writer(file_non_blocking)
-        .with_writer(stdout_non_blocking)
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "drn_ie_server=debug,tower_http=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let span = span!(Level::INFO, "bootstrap");
-    let _guard = span.enter();
-
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
-                .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
-        );
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    event!(Level::INFO, "Application will listen at 3000.");
-
-    axum::serve(listener, app.into_make_service())
+    let app = Router::new().nest_service("/", ServeDir::new("public"));
+    let port = 3001;
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.layer(TraceLayer::new_for_http()))
         .await
         .unwrap();
 }
